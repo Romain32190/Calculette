1c17400f816919e23f2c6ac58b055d6cc5bc7c47
Romain32190 <romain.lafargue36@gmail.com>
2017-03-13 17:21:52 +0100

    Modification du fichier js et ajout de jquery

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,14 +1,63 @@
-function Resultat() 
-{ 
-document.calculatrice.affichage.value = eval(document.calculatrice.affichage.value) 
-} 
+    $("input").on("click", function(evenement) {
+    var valeur = $(this).val();
+    console.log(valeur);
+    $("#resultat").val(valeur);
+});
 
-function Supprim() 
-{ 
-document.calculatrice.affichage.value = "" 
-} 
+//Additions//
 
-function Ajout(Signe) 
-{ 
-document.calculatrice.affichage.value += Signe 
-} 
+function additions(a, b){
+  console.log(a + b);
+
+};
+
+//Soustractions//
+
+function soustractions(a, b){
+  console.log(a - b);
+};
+
+//Multiplications//
+
+function multiplications(a, b){
+  console.log(a * b);
+};
+
+//Divisions//
+
+function divisions(a, b) {
+    if(b > 0){
+        console.log(a / b);
+        } else {
+            console.log(a / b);
+        }
+
+};
+
+var nombre1 = 0;
+var nombre2 = 0;
+
+$("add").click(function(){
+    operation = "additions";
+});  
+
+$("sous").click(function(){
+    operation = "soustractions";
+});  
+
+$("mul").click(function(){
+    operation = "multiplications";
+}); 
+
+$("div").click(function(){
+    operation = "divisions";
+}); 
+
+
+
+
+
+
+
+    
+
